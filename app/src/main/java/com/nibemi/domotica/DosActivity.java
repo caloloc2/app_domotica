561d69b101bdc287897e1ab706353939e675e209
caloloc2 <calolomino@gmail.com>
2021-07-25 21:06:13 -0500

    Actualizacion

--- app/src/main/java/com/nibemi/domotica/DosActivity.java
+++ app/src/main/java/com/nibemi/domotica/DosActivity.java
@@ -4,18 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DosActivity extends AppCompatActivity {
-    Button btn_regresar_dos;
+    Button btn_regresar_dos, btn_actualizar;
+    TextView NivelAct, NivelSet, TempAct, TempSet;
+
+    //Instancia a la base de datos
+    FirebaseDatabase fdb = FirebaseDatabase.getInstance();
+    //apuntamos al nodo que queremos leer
+    DatabaseReference myRef = fdb.getReference("NIVTEMP");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dos);
 
+        FirebaseApp.initializeApp(this);
+
+        NivelAct = (TextView)findViewById(R.id.NivelAct);
+        NivelSet = (TextView)findViewById(R.id.NivelSet);
+        TempAct = (TextView)findViewById(R.id.TempAct);
+        TempSet = (TextView)findViewById(R.id.TempSet);
+
         btn_regresar_dos = (Button)findViewById(R.id.btn_regresar_dos);
+        btn_actualizar = (Button)findViewById(R.id.btn_actualizar_nt);
 
         btn_regresar_dos.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +48,47 @@ public class DosActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
+        });
+
+        btn_actualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nivels = NivelSet.getText().toString();
+                String temps = TempSet.getText().toString();
+
+                if ((!nivels.isEmpty()) && (!temps.isEmpty())){
+                    myRef.child("NivelSet").setValue(Integer.parseInt(nivels));
+                    myRef.child("TempSet").setValue(Integer.parseInt(temps));
+
+                    Toast.makeText(getApplicationContext(), "Valores Actualizados", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Los valores deben ser numericos y son obligatorios", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                String nivel_actual = dataSnapshot.child("NivelAct").getValue().toString();
+                String temp_actual = dataSnapshot.child("TempAct").getValue().toString();
+
+                String nivel_set = dataSnapshot.child("NivelSet").getValue().toString();
+                String temp_set = dataSnapshot.child("TempSet").getValue().toString();
+
+                NivelAct.setText(nivel_actual+" %");
+                TempAct.setText(temp_actual+ " °");
+
+                NivelSet.setText(nivel_set);
+                TempSet.setText(temp_set);
+            }
+            @Override
+            public void onCancelled(DatabaseError error){
+                Log.e("ERROR FIREBASE",error.getMessage());
+            }
+
         });
     }
 }
--- app/src/main/java/com/nibemi/domotica/TresActivity.java
+++ app/src/main/java/com/nibemi/domotica/TresActivity.java
@@ -4,16 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TresActivity extends AppCompatActivity {
     Button btn_regresar_tres;
+    CheckBox Luz1, Luz2, Luz3, Luz4, Rele1, Rele2, Entrada1, Entrada2, Entrada3, Entrada4, Buzzer;
+    TextView Potenciometro;
+    //Instancia a la base de datos
+    FirebaseDatabase fdb = FirebaseDatabase.getInstance();
+    //apuntamos al nodo que queremos leer
+    DatabaseReference myRef = fdb.getReference("IO");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tres);
+
+        Luz1 = (CheckBox)findViewById(R.id.Luz1);
+        Luz2 = (CheckBox)findViewById(R.id.Luz2);
+        Luz3 = (CheckBox)findViewById(R.id.Luz3);
+        Luz4 = (CheckBox)findViewById(R.id.Luz4);
+
+        Rele1 = (CheckBox)findViewById(R.id.Rele1);
+        Rele2 = (CheckBox)findViewById(R.id.Rele2);
+
+        Entrada1 = (CheckBox)findViewById(R.id.Entrada1);
+        Entrada2 = (CheckBox)findViewById(R.id.Entrada2);
+        Entrada3 = (CheckBox)findViewById(R.id.Entrada3);
+        Entrada4 = (CheckBox)findViewById(R.id.Entrada4);
+
+        Buzzer = (CheckBox)findViewById(R.id.Buzzer);
+
+        Potenciometro = (TextView) findViewById(R.id.Potenciometro);
 
         btn_regresar_tres = (Button)findViewById(R.id.btn_regresar_tres);
 
@@ -25,5 +58,227 @@ public class TresActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        Luz1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Luz1", 1);
+                }else{
+                    IOSetValue("Luz1", 0);
+                }
+            }
+        });
+
+        Luz2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Luz2", 1);
+                }else{
+                    IOSetValue("Luz2", 0);
+                }
+            }
+        });
+
+        Luz3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Luz3", 1);
+                }else{
+                    IOSetValue("Luz3", 0);
+                }
+            }
+        });
+
+        Luz4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Luz4", 1);
+                }else{
+                    IOSetValue("Luz4", 0);
+                }
+            }
+        });
+
+        Rele1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Rele1", 1);
+                }else{
+                    IOSetValue("Rele1", 0);
+                }
+            }
+        });
+
+        Rele2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Rele2", 1);
+                }else{
+                    IOSetValue("Rele2", 0);
+                }
+            }
+        });
+
+        Buzzer.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Buzzer", 1);
+                }else{
+                    IOSetValue("Buzzer", 0);
+                }
+            }
+        });
+
+        Entrada1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Entrada1", 1);
+                }else{
+                    IOSetValue("Entrada1", 0);
+                }
+            }
+        });
+
+        Entrada2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Entrada2", 1);
+                }else{
+                    IOSetValue("Entrada2", 0);
+                }
+            }
+        });
+
+        Entrada3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Entrada3", 1);
+                }else{
+                    IOSetValue("Entrada3", 0);
+                }
+            }
+        });
+
+        Entrada4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    IOSetValue("Entrada4", 1);
+                }else{
+                    IOSetValue("Entrada4", 0);
+                }
+            }
+        });
+
+        myRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                String val_luz1 = dataSnapshot.child("Luz1").getValue().toString();
+                String val_luz2 = dataSnapshot.child("Luz2").getValue().toString();
+                String val_luz3 = dataSnapshot.child("Luz3").getValue().toString();
+                String val_luz4 = dataSnapshot.child("Luz4").getValue().toString();
+
+                if (val_luz1.equals("1")){
+                    Luz1.setChecked(true);
+                }else{
+                    Luz1.setChecked(false);
+                }
+
+                if (val_luz2.equals("1")){
+                    Luz2.setChecked(true);
+                }else{
+                    Luz2.setChecked(false);
+                }
+
+                if (val_luz3.equals("1")){
+                    Luz3.setChecked(true);
+                }else{
+                    Luz3.setChecked(false);
+                }
+
+                if (val_luz4.equals("1")){
+                    Luz4.setChecked(true);
+                }else{
+                    Luz4.setChecked(false);
+                }
+
+                String val_rele1 = dataSnapshot.child("Rele1").getValue().toString();
+                String val_rele2 = dataSnapshot.child("Rele2").getValue().toString();
+
+                if (val_rele1.equals("1")){
+                    Rele1.setChecked(true);
+                }else{
+                    Rele1.setChecked(false);
+                }
+
+                if (val_rele2.equals("1")){
+                    Rele2.setChecked(true);
+                }else{
+                    Rele2.setChecked(false);
+                }
+
+                String val_buzzer = dataSnapshot.child("Buzzer").getValue().toString();
+
+                if (val_buzzer.equals("1")){
+                    Buzzer.setChecked(true);
+                }else{
+                    Buzzer.setChecked(false);
+                }
+
+                String val_entrada1 = dataSnapshot.child("Entrada1").getValue().toString();
+                String val_entrada2 = dataSnapshot.child("Entrada2").getValue().toString();
+                String val_entrada3 = dataSnapshot.child("Entrada3").getValue().toString();
+                String val_entrada4 = dataSnapshot.child("Entrada4").getValue().toString();
+
+                if (val_entrada1.equals("1")){
+                    Entrada1.setChecked(true);
+                }else{
+                    Entrada1.setChecked(false);
+                }
+
+                if (val_entrada2.equals("1")){
+                    Entrada2.setChecked(true);
+                }else{
+                    Entrada2.setChecked(false);
+                }
+
+                if (val_entrada3.equals("1")){
+                    Entrada3.setChecked(true);
+                }else{
+                    Entrada3.setChecked(false);
+                }
+
+                if (val_entrada4.equals("1")){
+                    Entrada4.setChecked(true);
+                }else{
+                    Entrada4.setChecked(false);
+                }
+
+                String val_potenciometro = dataSnapshot.child("Potenciometro").getValue().toString();
+
+                Potenciometro.setText(val_potenciometro);
+            }
+            @Override
+            public void onCancelled(DatabaseError error){
+                Log.e("ERROR FIREBASE",error.getMessage());
+            }
+
+        });
+    }
+
+    private void IOSetValue(String tag, int valor) {
+        myRef.child(tag).setValue(valor);
     }
 }
